f3fa01f6f2757544f54f281620d78942a24f8640
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-12 09:57:13 -0500

    Database is connected, base structure added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
+
+let dbLocation = central(5);
+
+dbLocation.then((db)=>{
+  console.log(`The user is in ${db}`)
+})
+
+
 function getUserData(id) {
   const dbs = {
     db1: db1,
@@ -8,3 +16,5 @@ function getUserData(id) {
     db3: db3
   };
 }
+
+
