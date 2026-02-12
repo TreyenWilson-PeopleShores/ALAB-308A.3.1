12ac1cbf6062c7980cc75ce549a93d703376d68a
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-12 11:10:17 -0500

    Figured out basic usage

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import { central, db1, db2, db3, vault } from "./databases.js";
 
 let dbLocation = central(5);
 
-dbLocation.then((db)=>{
+dbLocation.then(function(db){
   console.log(`The user is in ${db}`)
 })
 
+//getUserData(1)
+
+
+ /*new Promise(function (resolve, reject){
+  console.log("Initial")
+  return resolve()
+})
+  .then(function (){
+    throw new Error("Something failed");
+    console.log("Do this")
+  })*/
+
+
+    let name = await vault(8);
+    //db1 needs 1 - 4
+    //db2 needs 4 - 7
+    //db3 7 and up
+    // db has company info
+    // vault has personal info
+    console.log("Test", name);
+
+  //
+  //
+
+getUserData(5)
 
 function getUserData(id) {
   const dbs = {
@@ -15,6 +40,29 @@ function getUserData(id) {
     db2: db2,
     db3: db3
   };
+  console.log(dbs);
+  //const returnedValue = await central(id);
+  //console.log(returnedValue);
+  return central(id).then(function(db){
+    console.log(db);
+    return db;
+  })
+  console.log(db);
 }
 
 
+
+
+
+
+
+//const myPromise = new Promise((resolve, reject) => {
+  //  setTimeout(() => {
+    //    resolve("Guess this worked!");
+   // }, 1000);
+//});
+
+//getUserData(5)
+//  .then(function(x){
+//    console.log(x);
+// })
